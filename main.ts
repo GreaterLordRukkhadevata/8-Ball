d04be7bba3902089416f7fa1373487e2e024ecef
GreaterLordRukkhadevata <112982431+GreaterLordRukkhadevata@users.noreply.github.com>
2022-09-13 12:38:10 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    Random_Number = randint(0, 3)
+    if (Random_Number == 0) {
+        basic.clearScreen()
+        basic.showString("Answer Unclear")
+    } else if (Random_Number == 1) {
+        basic.clearScreen()
+        basic.showString("Yes")
+    } else {
+        basic.clearScreen()
+        basic.showString("no")
+    }
+})
+let Random_Number = 0
+basic.showString("Ask a Question!")
+basic.showNumber(8)
+basic.forever(function () {
+	
+})
